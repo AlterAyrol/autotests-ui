7e75a912f960cc50d0e594be5d8f1768249983c1
AlterAyrol <lazarev.reach@gmail.com>
2025-05-30 09:00:26 +0300

    Знакомство с PageObject

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,5 +1,5 @@
 import pytest
-from playwright.sync_api import expect, sync_playwright
+from pages.login_page import LoginPage
 
 
 @pytest.mark.parametrize("email, password", [
@@ -11,25 +11,9 @@ from playwright.sync_api import expect, sync_playwright
     "Invalid email and empty password",
     "Empty email and invalid password"
 ])
-def test_wrong_email_or_password_authorization(email: str, password: str):
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        page = browser.new_page()
-
-        # Переходим на страницу
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
-
-        # Заполняем поле email
-        email_input = page.get_by_test_id('login-form-email-input').locator('input')
-        email_input.fill(email)
-        # Заполняем поле пароль
-        password_input = page.get_by_test_id('login-form-password-input').locator('input')
-        password_input.fill(password)
-        # Нажимаем на кнопку Login
-        login_button = page.get_by_test_id('login-page-login-button')
-        login_button.click()
-        # Проверяем сообщение об ошибке
-        wrong_email_or_password_alert = page.get_by_test_id('login-page-wrong-email-or-password-alert')
-        expect(wrong_email_or_password_alert).to_be_visible()
-        expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
+def test_wrong_email_or_password_authorization(login_page: LoginPage, email: str, password: str):
+    login_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+    login_page.fill_login_form(email=email, password=password)
+    login_page.click_login_button()
+    login_page.check_visible_wrong_email_or_password_alert()
 
