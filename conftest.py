3bc4ef249987bfe5dba5245f35ca7e7b80981a99
AlterAyrol <AlterAyrol@users.noreply.github.com>
2025-06-10 06:56:09 +0000

    deploy: b5508ebc7b085785ef545031ea71167acd6bc9f0

--- conftest.py
+++
@@ -1,6 +0,0 @@
-pytest_plugins = (
-    "fixtures.browsers",
-    "fixtures.allure",  # Добавили плагин для генерации в аллюр переменных окружения
-    "fixtures.pages",  # Подключаем фикстуры страниц
-)
-
